38ff303239f338b19377d7c757f189e1f284b564
asean0112 <asean0112@gmail.com>
2018-06-26 20:55:21 +0800

    0313

---
+++ 0313.py
@@ -0,0 +1,209 @@
+
+# coding: utf-8
+
+# # Getting started with Python
+
+# # Variables
+
+# In[1]:
+
+
+i=4
+
+
+# In[2]:
+
+
+type(i)
+
+
+# In[3]:
+
+
+f = 3.4
+
+
+# In[4]:
+
+
+type(f)
+
+
+# In[5]:
+
+
+b = True
+
+
+# In[6]:
+
+
+hungry = True 
+sleep = False
+
+
+# In[7]:
+
+
+not hungry
+
+
+# In[8]:
+
+
+hungry and sleep
+
+
+# In[9]:
+
+
+hungry or sleep
+
+
+# In[10]:
+
+
+type(b)
+
+
+# In[11]:
+
+
+s = "This is a string"
+
+
+# In[12]:
+
+
+type(s)
+
+
+# In[13]:
+
+
+1+2
+
+
+# In[14]:
+
+
+1 -2
+
+
+# # List
+
+# In[15]:
+
+
+4*5
+
+
+# In[16]:
+
+
+7/5
+
+
+# In[17]:
+
+
+3 ** 2
+
+
+# In[20]:
+
+
+a = [1,2,3,4,5,6]
+
+
+# In[21]:
+
+
+print(a)
+
+
+# In[22]:
+
+
+type(a)
+
+
+# In[23]:
+
+
+len(a)
+
+
+# In[24]:
+
+
+a[0]
+
+
+# In[25]:
+
+
+a[4]
+
+
+# In[26]:
+
+
+a[0:2]
+
+
+# In[27]:
+
+
+a[1:]
+
+
+# In[28]:
+
+
+a[:3]
+
+
+# In[31]:
+
+
+a[:-1]
+
+
+# # Advanced python types
+
+# In[32]:
+
+
+I = (3,1,2)
+
+
+# In[33]:
+
+
+print(I)
+
+
+# In[34]:
+
+
+type(I)
+
+
+# In[35]:
+
+
+d = {'foo':1,'bar':2.3,'s':'my dictionary'}
+
+
+# In[36]:
+
+
+type(d)
+
+
+# In[37]:
+
+
+d['foo']
+
